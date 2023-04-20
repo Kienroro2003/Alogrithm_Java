0ca2bc7101aff9349b742d8412c8f52c57898ed4
Kienroro2003 <kienroro281@gmail.com>
2023-04-20 13:22:40 +0700

    update

--- src/BinaryStringEnumeration.java
+++ src/BinaryStringEnumeration.java
@@ -1,19 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BinaryStringEnumeration {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/data/BSTR.INP"));
+        PrintWriter writer = new PrintWriter("src/data/BSTR.OUT");
         int n =  Integer.parseInt(bufferedReader.readLine().trim());
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print(array[i]+ " ");
-            i = n;
-            while (i > 0 && array[i] == 1){
-
+        int[] arr = new int[n];
+        int i = 0;
+        do{
+            for (i = 0; i < n; i++) {
+                writer.print(arr[i] + " ");
             }
-        }
+            writer.println();
+            for (i = n - 1; i >= 0 && arr[i] == 1  ; i--) {
+            }
+            if(i < 0)break;
+            arr[i++] = 1;
+            for (; i < n ; i++) {
+                arr[i] = 0;
+            }
+        }while (i != 0);
+        bufferedReader.close();
+        writer.flush();
+        writer.close();
     }
 }
