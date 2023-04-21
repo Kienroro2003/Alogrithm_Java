3a87869b94e5679a6ba13b07c7b169c45f5a68c9
Kienroro2003 <kienroro281@gmail.com>
2023-04-21 11:42:17 +0700

    update

--- src/BinaryStringEnumeration.java
+++ src/BinaryStringEnumeration.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class BinaryStringEnumeration {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader("src/data/BSTR.INP"));
-        PrintWriter writer = new PrintWriter("src/data/BSTR.OUT");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/data/BSTR.OUT"));
         int n =  Integer.parseInt(bufferedReader.readLine().trim());
         int[] arr = new int[n];
         int i = 0;
         do{
             for (i = 0; i < n; i++) {
-                writer.print(arr[i] + " ");
+                bufferedWriter.write(arr[i] + " ");
             }
-            writer.println();
+            bufferedWriter.newLine();
             for (i = n - 1; i >= 0 && arr[i] == 1  ; i--) {
             }
             if(i < 0)break;
@@ -23,7 +23,8 @@ public class BinaryStringEnumeration {
             }
         }while (i != 0);
         bufferedReader.close();
-        writer.flush();
-        writer.close();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        System.out.println("Ghi file hoan tat");
     }
 }
